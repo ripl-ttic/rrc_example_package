f1cbdeb13067b96cbc2501dff5947ae6ef47a4fd
Chip Schaff <cbschaff@ttic.edu>
2020-10-16 13:38:09 -0500

    add an option to run without a back end

---
+++ run_no_backend.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Execute a submission"""
+import argparse
+import os
+import signal
+import subprocess
+import tempfile
+import time
+import json
+import traceback
+import socket
+import logging
+import pathlib
+import sys
+import shutil
+from run_in_simulation import LocalExecutionConfig, SubmissionRunner
+
+
+def main():
+    log_handler = logging.StreamHandler(sys.stdout)
+    logging.basicConfig(
+        format="[SUBMISSION %(levelname)s %(asctime)s] %(message)s",
+        level=logging.DEBUG,
+        handlers=[log_handler],
+    )
+
+    config = LocalExecutionConfig()
+    runner = SubmissionRunner(config)
+    try:
+        with tempfile.TemporaryDirectory(
+            prefix="run_submission-"
+        ) as ws_dir:
+            logging.info("Use temporary workspace %s", ws_dir)
+
+            user_returncode = None
+
+            # create "src" directory and cd into it
+            src_dir = os.path.join(ws_dir, "src")
+            os.mkdir(src_dir)
+            os.chdir(src_dir)
+
+            runner.clone_user_repository()
+            runner.load_goal(src_dir)
+            runner.store_info()
+            os.chdir(ws_dir)
+            runner.build_workspace(ws_dir)
+
+            user_returncode = runner.run_user_code(ws_dir)
+
+            runner.store_report(False, user_returncode)
+
+            logging.info("Finished.")
+    except Exception as e:
+        logging.critical("FAILURE: %s", e)
+
+        # FIXME just for debugging, remove later
+        traceback.print_exc()
+
+        error_report_file = os.path.join(
+            runner.config.host_output_dir, "error_report.txt"
+        )
+        with open(error_report_file, "w") as fh:
+            fh.write(
+                "Submission failed with the following error:\n{}\n".format(
+                    e
+                )
+            )
+
+
+if __name__ == "__main__":
+    main()
