9ac2192d31cebc54e3b456ae5d75d2895fffa643
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-10-21 22:07:32 +0900

    trial: stay at the original position

--- scripts/run_physical_episode.py
+++ scripts/run_physical_episode.py
@@ -5,6 +5,38 @@ from code.make_env import make_training_env
 from trifinger_simulation.tasks import move_cube
 from code.env.cube_env import ActionType
 
+from code.make_env import get_reward_fn, get_initializer, get_termination_fn
+from code.env.cube_env import RealRobotCubeEnv, ActionType
+from code.wrappers import *
+
+def make_pure_env(cube_goal_pose, goal_difficulty, action_space, frameskip=1,
+                      sim=False, visualization=False, reward_fn=None,
+                      termination_fn=None, initializer=None, episode_length=120000,
+                      residual=False, rank=0, monitor=False):
+    is_level_4 = goal_difficulty == 4
+    reward_fn = get_reward_fn(reward_fn)
+    initializer = get_initializer(initializer)
+    termination_fn = get_termination_fn(termination_fn)
+    if action_space not in ['torque', 'position', 'torque_and_position', 'position_and_torque']:
+        raise ValueError(f"Unknown action space: {action_space}.")
+    if action_space == 'torque':
+        action_type = ActionType.TORQUE
+    elif action_space in ['torque_and_position', 'position_and_torque']:
+        action_type = ActionType.TORQUE_AND_POSITION
+    else:
+        action_type = ActionType.POSITION
+    env = RealRobotCubeEnv(cube_goal_pose,
+                           goal_difficulty,
+                           action_type=action_type,
+                           frameskip=frameskip,
+                           sim=sim,
+                           visualization=visualization,
+                           reward_fn=reward_fn,
+                           termination_fn=termination_fn,
+                           initializer=initializer,
+                           episode_length=episode_length)
+    env.seed(seed=rank)
+    env.action_space.seed(seed=rank)
 
 def main():
     goal = move_cube.sample_goal(3)
@@ -25,26 +57,38 @@ def main():
         'rank': 0,
         'episode_length': 20000
     }
-    env = make_training_env(goal_dict, difficulty, sim=False, visualization=False,
-                            **eval_config)
+    # env = make_training_env(goal_dict, difficulty, sim=False, visualization=False,
+    #                         **eval_config)
+    env = make_pure_env(goal_dict, difficulty, sim=False, visualization=False,
+                        **eval_config)
 
     obs = env.reset()
     done = False
     accumulated_reward = 0
     if env.action_type == ActionType.TORQUE_AND_POSITION:
-        zero_action = {
-            'torque': (env.action_space['torque'].sample() * 0).astype(np.float64),
-            'position': (env.action_space['position'].sample() * 0).astype(np.float64)
-        }
-        assert zero_action['torque'].dtype == np.float64
-        assert zero_action['position'].dtype == np.float64
+        # zero_action = {
+        #     'torque': (env.action_space['torque'].sample() * 0).astype(np.float64),
+        #     'position': (env.action_space['position'].sample() * 0).astype(np.float64)
+        # }
+        # assert zero_action['torque'].dtype == np.float64
+        # assert zero_action['position'].dtype == np.float64
+        zero_action = env._initial_action
     else:
-        zero_action = np.array(env.action_space.sample() * 0).astype(np.float64)
-        assert zero_action.dtype == np.float64
+        # zero_action = np.array(env.action_space.sample() * 0).astype(np.float64)
+        # assert zero_action.dtype == np.float64
+        zero_action = env._initial_action
 
+    import random
+    import time
+    counter = 0
     while not done:
+        # create intentional delay
+        if counter > 100 and random.random() < 0.3:
+            time.sleep(0.1)
+
         obs, reward, done, info = env.step(zero_action)
         accumulated_reward += reward
+        counter += 1
     print("Accumulated reward: {}".format(accumulated_reward))
 
 
