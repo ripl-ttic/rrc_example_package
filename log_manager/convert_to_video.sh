a33a5f871cff549aef9de8e696da022fc0fa958f
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-10-27 20:54:52 +0900

    add utility scripts

---
+++ log_manager/convert_to_video.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+
+logdir=$1
+jobid=$2
+server_logdir="log-${jobid}"
+echo $server_logdir
+scp -r ${logdir} takumasus:workspace/rrc_phase_2/log_manager/${server_logdir}
+ssh -t takumasus "cd ~/workspace/rrc_phase_2/log_manager && ./make_video.sh ~/Downloads/rrc_phase_2.sif ${server_logdir}"
+scp takumasus:workspace/rrc_phase_2/log_manager/${server_logdir}/video.mp4 ${logdir}/video.mp4
+scp takumasus:workspace/rrc_phase_2/log_manager/${server_logdir}/replay.mp4 ${logdir}/replay.mp4
