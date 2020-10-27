a3ca9b1eba96125eaed17c36790dbd9cccaeafdb
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-10-27 19:52:48 +0900

    tiny fix

--- scripts/test_motion.py
+++ scripts/test_motion.py
@@ -23,9 +23,9 @@ def main():
         'initializer': f'task{difficulty}_init',
         'monitor': False,
         'rank': 0,
-        'episode_length': 20000
+        'episode_length': 100000
     }
-    env = make_training_env(goal_dict, difficulty, sim=True, visualization=True,
+    env = make_training_env(goal_dict, difficulty, sim=False, visualization=False,
                             **eval_config)
 
     obs = env.reset()
