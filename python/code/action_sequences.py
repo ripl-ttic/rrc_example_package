5cb5b0a500a2f7cc10630f9749a5cb507f470fdd
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-10-29 14:42:37 +0900

    use complete_keypoints for smoother motion

--- python/code/action_sequences.py
+++ python/code/action_sequences.py
@@ -4,25 +4,30 @@ from code.const import *
 from code.align_rotation import pitch_rotation_axis_and_angle, project_cube_xy_plane
 from scipy.spatial.transform import Rotation as R
 import numpy as np
+from code import utils
 
 class ScriptedActions:
-    def __init__(self, cube_tip_positions, orientation=None):
+    def __init__(self, env, cube_tip_positions, orientation=None):
+        self.env = env
+        self.ik_utils = utils.IKUtils(env)
         self.cube_tip_positions = cube_tip_positions
         self.orientation = orientation
         self.tip_positions_list = []
         self._markers = set()
 
-    def add_grasp(self, obs, coef=0.9):
+    def add_grasp(self, obs, coef=0.9, vis_markers=None):
         grasp_target_cube_positions = self.cube_tip_positions * coef
         target_tip_positions = apply_transform(obs['object_position'], obs['object_orientation'], grasp_target_cube_positions)
-        self.tip_positions_list.append(target_tip_positions)
+        tip_pos_sequence = utils.complete_keypoints(self.ik_utils, self.get_last_tippos(obs), target_tip_positions, unit_length=0.004, vis_markers=vis_markers)
+        self.tip_positions_list += tip_pos_sequence
 
     def add_liftup(self, obs, height=0.0425, coef=0.6):
         grasp_target_cube_positions = self.cube_tip_positions * coef
         target_tip_positions = apply_transform(
             obs['object_position'] + np.array([0, 0, height]),
             obs['object_orientation'], grasp_target_cube_positions)
-        self.tip_positions_list.append(target_tip_positions)
+        tip_pos_sequence = utils.complete_keypoints(self.ik_utils, self.get_last_tippos(obs), target_tip_positions, unit_length=0.004)
+        self.tip_positions_list += tip_pos_sequence
 
     def add_pitch_rotation(self, obs, rotate_axis, rotate_angle, coef=0.6, vis_markers=None):
         grasp_target_cube_positions = self.cube_tip_positions * coef
@@ -30,7 +35,8 @@ class ScriptedActions:
         target_tip_positions = apply_transform(
             obs['object_position'] + np.array([0, 0, 0.0425]),
             self.orientation, grasp_target_cube_positions)
-        self.tip_positions_list.append(target_tip_positions)
+        tip_pos_sequence = utils.complete_keypoints(self.ik_utils, self.get_last_tippos(obs), target_tip_positions, unit_length=0.004, vis_markers=vis_markers)
+        self.tip_positions_list += tip_pos_sequence
         if vis_markers is not None:
             if 'pitch' in self._markers:
                 vis_markers.remove()
@@ -43,7 +49,8 @@ class ScriptedActions:
         angle_clipped = np.clip(angle, -step_angle, step_angle)
         self.orientation = ( R.from_euler('Z', angle_clipped) * R.from_quat(obs['object_orientation'])).as_quat()
         target_tip_positions = apply_transform(obs['object_position'], self.orientation, grasp_target_cube_positions)
-        self.tip_positions_list.append(target_tip_positions)
+        tip_pos_sequence = utils.complete_keypoints(self.ik_utils, self.get_last_tippos(obs), target_tip_positions, unit_length=0.004, vis_markers=vis_markers)
+        self.tip_positions_list += tip_pos_sequence
         if vis_markers is not None:
             if 'yaw' in self._markers:
                 vis_markers.remove()
@@ -57,7 +64,8 @@ class ScriptedActions:
         target_tip_positions = apply_transform(obs['object_position'],
                                             self.orientation,
                                             grasp_target_cube_positions)
-        self.tip_positions_list.append(target_tip_positions)
+        tip_pos_sequence = utils.complete_keypoints(self.ik_utils, self.get_last_tippos(obs), target_tip_positions, unit_length=0.004)
+        self.tip_positions_list += tip_pos_sequence
 
     def add_release(self, obs, coef=2.0, min_allowed_height=0.02):
         assert self.orientation is not None
@@ -66,7 +74,13 @@ class ScriptedActions:
         min_height = target_tip_positions[:, 2].min()
         if min_height < min_allowed_height:
             target_tip_positions[:, 2] += (min_allowed_height - min_height)
-        self.tip_positions_list.append(target_tip_positions)
+        tip_pos_sequence = utils.complete_keypoints(self.ik_utils, self.get_last_tippos(obs), target_tip_positions, unit_length=0.004)
+        self.tip_positions_list += tip_pos_sequence
+
+    def get_last_tippos(self, obs):
+        if not self.tip_positions_list:
+            return obs['robot_tip_positions']
+        return self.tip_positions_list[-1]
 
     def get_tip_sequence(self):
         return self.tip_positions_list
