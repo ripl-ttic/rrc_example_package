fbf962d639567f9135775d8c2cd930758a67a3f3
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-11-02 14:52:15 +0900

    bugfix

--- python/code/wrappers.py
+++ python/code/wrappers.py
@@ -168,6 +168,11 @@ class ResidualLearningFCWrapper(gym.Wrapper):
 
     def reset(self):
         obs = self.env.reset()
+        self.env.register_custom_log('init_cube_pos', obs['object_position'])
+        self.env.register_custom_log('init_cube_ori', obs['object_orientation'])
+        self.env.register_custom_log('goal_pos', obs['goal_object_position'])
+        self.env.register_custom_log('goal_ori', obs['goal_object_orientation'])
+        self.env.save_custom_logs()
 
         # flip the cube
         if self.is_level_4:
